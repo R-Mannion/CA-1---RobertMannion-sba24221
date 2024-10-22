68fbfe9aa2d3d6f6b7e0622ddc611264ded3397e
R-Mannion <sba24221@student.cct.ie>
2024-10-22 16:44:33 +0100

    Commented imports
    Added comments to imports

--- CA1 Robert Mannion/src/ca1/robert/mannion/customerClass.java
+++ CA1 Robert Mannion/src/ca1/robert/mannion/customerClass.java
@@ -4,11 +4,11 @@
  */
 package ca1.robert.mannion;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.BufferedReader; //To read text from input file and increase effeciency with file reader
+import java.io.BufferedWriter; //To write text in output file
+import java.io.FileReader;      //To read text from input file
+import java.io.FileWriter;      //To write text for output file
+import java.io.IOException;     //To catch errors from input/output
 
 /**
  *
