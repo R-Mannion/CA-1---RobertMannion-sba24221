1ad858b8400330dc04d07c0ceac7f9109b773748
R-Mannion <sba24221@student.cct.ie>
2024-10-22 12:48:53 +0100

    Created NetBeans file
    Created file: Ca1 Robert Mannion in NetBeans

---
+++ CA1 Robert Mannion/src/ca1/robert/mannion/CA1RobertMannion.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ca1.robert.mannion;
+
+/**
+ *
+ * @author manni
+ */
+public class CA1RobertMannion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
